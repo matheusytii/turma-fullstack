1fedf9c30e5cde424cf85ebfa40b211a5650d1ff
matheus ramos <matheusramosyti1401@hotmail.com>
2025-06-12 20:58:51 -0300

    Entrada nome usuario

--- app.java
+++ app.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class app {
 
     public static void main(String[] args) {
-        System.out.println("hello, word!!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu nome : ");
+        String nome = sc.nextLine();
+        
+        sc.close();
+        System.out.println("bem vindo(a) " + nome);
+
     }
 }
