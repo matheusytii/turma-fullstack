db06705cc90419bf13f2565babd892634d02a061
matheus ramos <matheusramosyti1401@hotmail.com>
2025-06-17 20:34:58 -0300

    adiçao de nomes de variaveis novos

--- Media.java
+++ Media.java
@@ -5,17 +5,17 @@ public class Media {
         Scanner sc = new Scanner (System.in);
         
         System.out.println("digite o valor de x: ");
-        int x = sc.nextInt();
+        int num1= sc.nextInt();
         
         System.out.println("digite o valor de y: ");
-        int y = sc.nextInt();
+        int num2= sc.nextInt();
         
         System.out.println("digite o valor de Z: ");
-        int z= sc.nextInt();
+        int num3= sc.nextInt();
         
         sc.close();
         
-        double media = (x + y + z ) /3.0;
+        double media = (num1 + num2 + num3 ) /3.0;
 
         System.out.printf(" A media de X e Y eh : %.2f\n", media);
     }
