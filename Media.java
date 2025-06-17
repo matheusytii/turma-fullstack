e09cff704eef6fdbde0227899e2843e23abf50a7
matheus ramos <matheusramosyti1401@hotmail.com>
2025-06-17 19:57:27 -0300

    calculo da Media de dois numeros

---
+++ Media.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.println("digite o valor de x: ");
+        int x = sc.nextInt();
+        
+        System.out.println("digite o valor de y: ");
+        int y = sc.nextInt();
+        
+        sc.close();
+        
+        double media = (x + y ) /2.0;
+
+        System.out.printf(" A media de X e Y eh : %.2f\n", media);
+    }
+}
+
+    
+
